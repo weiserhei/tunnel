e576599741f14d5acbfdbdc1cf02d41203bc36f7
weiserhei <falk@thiele-medien.de>
2020-08-11 23:57:56 +0200

    add webpack-bundle-analzer

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,12 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 // const CopyPlugin = require('copy-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
+    new BundleAnalyzerPlugin(),
     // new CopyPlugin({
     //   patterns: [
     //     { from: 'src/models', to: 'models' },
