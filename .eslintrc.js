245729718707c15572074fbc034e830c4ab2408f
weiserhei <falk@thiele-medien.de>
2020-08-12 18:58:58 +0200

    add prettier-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,13 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2020": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "ignorePatterns": ["/*.js"],
-    "rules": {
-    },
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  ignorePatterns: ["/*.js"],
+  rules: {},
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
 };
